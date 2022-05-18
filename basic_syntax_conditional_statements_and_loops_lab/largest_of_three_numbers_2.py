ca7648ddeeed9ecce38e68618b6418db44b16b3e
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-05-18 18:09:50 +0300

    Update largest_of_three_numbers_2.py
    The problem is solved with - list()

--- basic_syntax_conditional_statements_and_loops_lab/largest_of_three_numbers_2.py
+++ basic_syntax_conditional_statements_and_loops_lab/largest_of_three_numbers_2.py
@@ -1,15 +1,6 @@
-# my_list = []
-#
-# my_list.append(int(input()))
-# my_list.append(int(input()))
-# my_list.append(int(input()))
-# print(max(my_list))
-
 my_list = []
 
 my_list.append(int(input()))
 my_list.append(int(input()))
 my_list.append(int(input()))
-for num in range(0, len(my_list) - 1):
-    my_list.remove(min(my_list))
-print("".join(map(str, my_list)))
+print(max(my_list))
