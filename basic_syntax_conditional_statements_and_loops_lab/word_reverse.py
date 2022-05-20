9ca837c846b35aba07a42523707c195c6da53acd
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-05-20 18:25:47 +0300

    Update word_reverse.py

--- basic_syntax_conditional_statements_and_loops_lab/word_reverse.py
+++ basic_syntax_conditional_statements_and_loops_lab/word_reverse.py
@@ -1,8 +1,5 @@
-# text = input()
-# length = len(text)
-# sliced_string = text[length::-1]
-# print(sliced_string)
-
 text = input()
-for i in range(len(text) - 1, -1, -1):
-    print(text[i], end="")
+length = len(text)
+sliced_string = text[length::-1]
+print(sliced_string)
+
