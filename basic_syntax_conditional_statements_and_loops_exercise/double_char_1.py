06dc21c1e701345d3598db2af0c757e8e2911d26
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-05-22 20:01:57 +0300

    Update double_char_1.py

--- basic_syntax_conditional_statements_and_loops_exercise/double_char_1.py
+++ basic_syntax_conditional_statements_and_loops_exercise/double_char_1.py
@@ -3,7 +3,7 @@ current_command = input()
 while current_command != "End":
     if current_command != "SoftUni":
         for char in current_command:
-            print(f"{char * 2}", end ="")
+            print(f"{char + char}", end ="")
         print()
     current_command = input()
 
