0cbccdf962dc9897c994834eea41721d88c70eff
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-05-22 20:29:10 +0300

    Create mutate_string_1.py

---
+++ basic_syntax_conditional_statements_and_loops_exercise/mutate_string_1.py
@@ -0,0 +1,11 @@
+first_string = input()
+second_string = input()
+last_string = first_string
+for symbol_index in range(len(second_string)):
+    left_part = second_string[:symbol_index + 1]  # [0:symbol+1:1]
+    right_part = first_string[symbol_index + 1:]  #[0:len(first_string):1]
+    current_string = left_part + right_part
+    if current_string == last_string:
+        continue
+    print(current_string)
+    last_string = current_string
