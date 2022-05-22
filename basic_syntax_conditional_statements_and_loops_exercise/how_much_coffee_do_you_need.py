1e5470d27c52f42416916edd8a602293258ca511
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-05-22 20:02:03 +0300

    Update how_much_coffee_do_you_need.py

--- basic_syntax_conditional_statements_and_loops_exercise/how_much_coffee_do_you_need.py
+++ basic_syntax_conditional_statements_and_loops_exercise/how_much_coffee_do_you_need.py
@@ -1,32 +1,16 @@
-# command = input()
-#
-# count_coffee = 0
-# while command != "END":
-#     if command == "coding" or command == "dog" or command == "cat" or command == "movie":
-#         count_coffee += 1
-#     elif command == "CODING" or command == "DOG" or command == "CAT" or command == "MOVIE":
-#         count_coffee += 2
-#     command = input()
-# if count_coffee > 5:
-#     print("You need extra sleep")
-# else:
-#     print(count_coffee)
-
 command = input()
 
 count_coffee = 0
 while command != "END":
-    if command == "coding" or command == "dog" or command == "cat" or \
-            command == "movie" or command == "CODING" or command == "DOG" or\
-            command == "CAT" or command == "MOVIE":
-        if command.islower():
-            count_coffee += 1
-        else:
-            command.isupper()
-            count_coffee += 2
+    if command == "coding" or command == "dog" or command == "cat" or command == "movie":
+        count_coffee += 1
+    elif command == "CODING" or command == "DOG" or command == "CAT" or command == "MOVIE":
+        count_coffee += 2
     command = input()
 if count_coffee > 5:
     print("You need extra sleep")
 else:
     print(count_coffee)
+
+
 
