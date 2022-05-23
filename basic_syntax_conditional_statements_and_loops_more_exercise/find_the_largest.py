2bce00e68690dd45990aa3aa71ff776cc5c96b03
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-05-23 13:20:31 +0300

    Update find_the_largest.py

--- basic_syntax_conditional_statements_and_loops_more_exercise/find_the_largest.py
+++ basic_syntax_conditional_statements_and_loops_more_exercise/find_the_largest.py
@@ -1,11 +1,5 @@
-# number = int(input())
-# num = str(number)
-#
-# m = sorted(num, reverse = True)
-# for d, digit in enumerate(m):
-#     print(digit, end="")
-#
 number = int(input())
+
 num = str(number)
 m = sorted(num, reverse = True)
 for i, digit in enumerate(m):
