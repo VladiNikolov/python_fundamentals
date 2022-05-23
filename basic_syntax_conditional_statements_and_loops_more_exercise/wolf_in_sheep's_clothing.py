a16870f0124bdc9fa8856b8eeeccf6f9a5e13574
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-05-23 16:51:40 +0300

    Update wolf_in_sheep's_clothing.py

--- basic_syntax_conditional_statements_and_loops_more_exercise/wolf_in_sheep's_clothing.py
+++ basic_syntax_conditional_statements_and_loops_more_exercise/wolf_in_sheep's_clothing.py
@@ -1,22 +1,10 @@
-# animals = input()
-# animals_list = animals.split(', ')
-# animals_list.reverse()
-# for index, animal in enumerate(animals_list):
-#     if animal == 'wolf' and index == 0:
-#         print('Please go away and stop eating my sheep')
-#     elif animal == 'wolf':
-#         print(f'Oi! Sheep number {index}! You are about to be eaten by a wolf!')
-#
-
 animals = input()
-animals_list = []
+animals_list = animals.split(', ')
+animals_list.reverse()
+for index, animal in enumerate(animals_list):
+    if animal == 'wolf' and index == 0:
+        print('Please go away and stop eating my sheep')
+    elif animal == 'wolf':
+        print(f'Oi! Sheep number {index}! You are about to be eaten by a wolf!')
 
-for animal in animals.split(","):
-    animals_list.append(str(animal).strip())
 
-farmer_position = len(animals_list) + 1
-if animals_list[-1].strip() == "wolf":
-    print("Please go away and stop eating my sheep")
-else:
-    animal_position = len(animals_list) - animals_list.index("wolf") - 1
-    print(f"Oi! Sheep number {animal_position}! You are about to be eaten by a wolf!")
