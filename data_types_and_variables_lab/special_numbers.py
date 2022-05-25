69487ad54f17b8643efc5de64d6c9a8850777de4
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-05-25 11:22:37 +0300

    Update special_numbers.py

--- data_types_and_variables_lab/special_numbers.py
+++ data_types_and_variables_lab/special_numbers.py
@@ -1,17 +1,3 @@
-# number = int(input())
-#
-# for n in range(1, number + 1):
-#     sum_digits = 0
-#     digits = n
-#     if digits > 0:
-#         sum_digits = sum_digits + digits % 10
-#         digits = int(digits / 10)
-#     if sum_digits + digits == 5 or sum_digits + digits == 7 or sum_digits + digits == 11:
-#         print(f"{n} -> True")
-#     else:
-#         print(f"{n} -> False")
-
-
 number = int(input())
 
 for num in range(1, number + 1):
