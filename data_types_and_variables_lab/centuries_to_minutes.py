3596efbe6e77cdf817289fb34b1eac0c58fe3c92
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-05-25 10:06:20 +0300

    Update centuries_to_minutes.py

--- data_types_and_variables_lab/centuries_to_minutes.py
+++ data_types_and_variables_lab/centuries_to_minutes.py
@@ -1,16 +1,8 @@
+import math
+
 centuries = int(input())
 year = centuries * 100
-days = int(year * 365.2422)
-hours = days * 24
+days = year * 365.2422
+hours = math.floor(days) * 24
 minutes = hours * 60
-
-print(f"{centuries} centuries = {year} years = {days} days = {hours} hours = {minutes} minutes")
-
-# import math
-#
-# centuries = int(input())
-# year = centuries * 100
-# days = year * 365.2422
-# hours = math.floor(days) * 24
-# minutes = hours * 60
-# print(f"{centuries} centuries = {year} years = {math.floor(days)} days = {hours} hours = {minutes} minutes")
+print(f"{centuries} centuries = {year} years = {math.floor(days)} days = {hours} hours = {minutes} minutes")
