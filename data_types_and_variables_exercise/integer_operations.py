972251ddf8cc312e8aa587030ccaddac77dae99b
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-05-26 18:15:38 +0300

    Update integer_operations.py

--- data_types_and_variables_exercise/integer_operations.py
+++ data_types_and_variables_exercise/integer_operations.py
@@ -1,12 +1,3 @@
-# first_number = int(input())
-# second_number = int(input())
-# third_number = int(input())
-# fourth_number = int(input())
-#
-# total_sum = first_number + second_number
-# total_sum = (total_sum // third_number) * fourth_number
-# print(int(total_sum))
-
 first_number = int(input())
 second_number = int(input())
 third_number = int(input())
