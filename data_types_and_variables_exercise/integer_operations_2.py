a301357b6963bcf0faf2445d73485e7250498788
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-05-26 18:15:20 +0300

    Create integer_operations_2.py

---
+++ data_types_and_variables_exercise/integer_operations_2.py
@@ -0,0 +1,8 @@
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+fourth_number = int(input())
+
+total_sum = first_number + second_number
+total_sum = (total_sum // third_number) * fourth_number
+print(int(total_sum))
