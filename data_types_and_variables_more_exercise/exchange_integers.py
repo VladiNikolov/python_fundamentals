50e1d32d0696a17e58254c6783db9082702a3ca1
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-05-30 22:31:51 +0300

    Update exchange_integers.py

--- data_types_and_variables_more_exercise/exchange_integers.py
+++ data_types_and_variables_more_exercise/exchange_integers.py
@@ -25,13 +25,4 @@
 
 #--------------------------------------
 
-a = int(input())
-b = int(input())
 
-b, a = b, a
-print("Before:")
-print(f"a = {a}")
-print(f"b = {b}")
-print("After:")
-print(f"a = {b}")
-print(f"b = {a}")
