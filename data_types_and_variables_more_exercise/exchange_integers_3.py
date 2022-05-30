d5c089ca1330f6171940081e9818a35c08207c88
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-05-30 22:41:37 +0300

    Create exchange_integers_3.py

---
+++ data_types_and_variables_more_exercise/exchange_integers_3.py
@@ -0,0 +1,12 @@
+a = int(input()) # a = 5
+b = int(input()) # b = 10
+
+a = a + b #a = 5, b = 10 ->a = 15
+b = a - b #a = 15, b = 10 ->b = 5
+a = a - b #a = 15, b = 5 ->b = 10
+print("Before:")
+print(f"a = {b}")
+print(f"b = {a}")
+print("After:")
+print(f"a = {a}")
+print(f"b = {b}")
