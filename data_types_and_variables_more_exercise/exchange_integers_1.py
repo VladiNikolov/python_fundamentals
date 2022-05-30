7b8f8f5dadd27af7319d048f3a27841289d43ea2
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-05-30 22:28:44 +0300

    Create exchange_integers_1.py

---
+++ data_types_and_variables_more_exercise/exchange_integers_1.py
@@ -0,0 +1,9 @@
+first_input = int(input())
+second_input = int(input())
+
+print(f"Before:")
+print(f"a = {first_input}")
+print(f"b = {second_input}")
+print(f"After:")
+print(f"a = {second_input}")
+print(f"b = {first_input}")
