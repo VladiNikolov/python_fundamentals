4e1291947729ff13daeef4435dfc9a5dbfbdf611
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-06-04 16:22:02 +0300

    Delete multiples_list_2.py

--- lists_basic_exercise/multiples_list_2.py
+++
@@ -1,9 +0,0 @@
-factor = int(input())
-count = int(input())
-
-my_list = []
-for number in range(1, count + 1):
-    current_number = number * factor
-    my_list.append(current_number)
-print(my_list)
-
