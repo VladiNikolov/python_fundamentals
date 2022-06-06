8754bccb9075c99a37998407178c6c21cd9be9d0
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-06-06 18:25:46 +0300

    Create zeros_to_back_3.py

---
+++ lists_basic_more_exercise/zeros_to_back_3.py
@@ -0,0 +1,11 @@
+input_string = input().split(", ")
+my_list_1 = []
+my_list_2 = []
+
+for string in input_string:
+    if int(string) != 0:
+        my_list_1.append(int(string))
+    else:
+        my_list_2.append(int(string))
+my_list_1.extend(my_list_2)
+print(my_list_1)
