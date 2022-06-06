b9b2e3915c6f3ab9cf53483953343b6494147d61
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-06-06 18:26:03 +0300

    Update zeros_to_back.py

--- lists_basic_more_exercise/zeros_to_back.py
+++ lists_basic_more_exercise/zeros_to_back.py
@@ -1,16 +1,3 @@
-# input_string = input().split(", ")
-# my_list_1 = []
-# my_list_2 = []
-#
-# for string in input_string:
-#     if int(string) != 0:
-#         my_list_1.append(int(string))
-#     else:
-#         my_list_2.append(int(string))
-# print(my_list_1 + my_list_2 )
-#
-#--------------------------------------
-#
 input_string = input().split(", ")
 my_list_1 = []
 my_list_2 = []
@@ -20,5 +7,5 @@ for string in input_string:
         my_list_1.append(int(string))
     else:
         my_list_2.append(int(string))
-my_list_1.extend(my_list_2)
-print(my_list_1)
+print(my_list_1 + my_list_2)
+
