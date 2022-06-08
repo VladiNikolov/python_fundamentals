c71c5323bbb8d875973809216a8df9a4d8a1cd75
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-06-08 12:04:23 +0300

    Update messaging.py

--- lists_basic_more_exercise/messaging.py
+++ lists_basic_more_exercise/messaging.py
@@ -15,13 +15,14 @@ for index, el in enumerate(string):
 
 for index in range(len(new_list)):
     if new_list[index] > len(last_list):
-        result = new_list[index] - len(last_list)
+        result = new_list[index] // len(last_list)
     else:
         result = new_list[index]
     second_list.append(last_list[result])
     last_list.remove(last_list[result])
 
 print("".join(second_list))
+
 
 
 
