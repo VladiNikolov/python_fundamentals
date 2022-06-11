e29f3c8341a85b0d696a5bca244c7f47669ed53b
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-06-11 19:20:32 +0300

    Update absolute_values_1.py

--- functions/absolute_values_1.py
+++ functions/absolute_values_1.py
@@ -2,8 +2,8 @@ def absolute_values(some_list):
     abs_list = []
     for element in range(len(some_list)):
         abs_list.append(abs(some_list[element]))
-    return abs_list
+    print(abs_list)
 
 input_number = input().split()
 input_number = [float(i) for i in input_number]
-print(absolute_values(input_number))
+absolute_values(input_number)
