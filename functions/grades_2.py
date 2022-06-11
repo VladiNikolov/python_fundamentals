5990951845fe730fe5d48ece91a15413a6f17279
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-06-11 16:28:33 +0300

    Create grades_2.py
    The problem is solved with - return

---
+++ functions/grades_2.py
@@ -0,0 +1,15 @@
+def grade_to_text(grade):
+    if 2 <= grade <= 2.99:
+        return "Fail"
+    elif 3 <= grade <= 3.49:
+        return "Poor"
+    elif 3.50 <= grade <= 4.49:
+        return "Good"
+    elif 4.50 <= grade <= 5.49:
+        return "Very Good"
+    elif 5.50 <= grade <= 6.00:
+        return "Excellent"
+
+
+grade_as_num = float(input())
+print(grade_to_text(grade_as_num))
