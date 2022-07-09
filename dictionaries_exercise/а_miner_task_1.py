15825c08b32d30700fe170687412cbf8606a5420
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-07-09 12:12:46 +0300

    Update а_miner_task_1.py

--- dictionaries_exercise/а_miner_task_1.py
+++ dictionaries_exercise/а_miner_task_1.py
@@ -5,9 +5,8 @@ while command != "stop":
     current_resource = command
     quantity = int(input())
     if current_resource not in resource_dict:
-        resource_dict[current_resource] = quantity
-    else:
-        resource_dict[current_resource] += quantity
+        resource_dict[current_resource] = 0
+    resource_dict[current_resource] += quantity
     command = input()
 
 for key in resource_dict.keys():
