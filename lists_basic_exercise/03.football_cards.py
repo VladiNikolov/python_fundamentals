2e2da772f32af4733c57ad1c56ba58ed87780692
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-08-09 08:49:30 +0300

    Create 03.football_cards.py

---
+++ lists_basic_exercise/03.football_cards.py
@@ -0,0 +1,23 @@
+input_players = input().split()
+list_team_a = []
+list_team_b = []
+game_was_terminated = False
+
+for i in range(1, 11 + 1):
+    list_team_a.append(f"A-{i}")
+    list_team_b.append(f"B-{i}")
+
+for player in input_players:
+    if player in list_team_a:
+        list_team_a.remove(player)
+    elif player in list_team_b:
+        list_team_b.remove(player)
+
+    if len(list_team_a) < 7 or len(list_team_b) < 7:
+        game_was_terminated = True
+        break
+
+print(f"Team A - {len(list_team_a)}; Team B - {len(list_team_b)}")
+if game_was_terminated:
+    print("Game was terminated")
+
