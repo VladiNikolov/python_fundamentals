b6e4e5ef77cda429f28925bdda44352abecc0d27
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-08-10 17:28:52 +0300

    Create 07.easter_gifts.py

---
+++ lists_basic_exercise/07.easter_gifts.py
@@ -0,0 +1,29 @@
+input_lines = input().split()
+
+command_lines = input()
+command = command_lines.split()
+while command_lines != "No Money":
+
+    if command[0] == "OutOfStock":
+        gift = command[1]
+        for i in range(len(input_lines)):
+            if input_lines[i] == gift:
+                input_lines[i] = "None"
+
+    if command[0] == "Required":
+        gift = command[1]
+        index = int(command[2])
+        if 0 <= index < len(input_lines):
+            input_lines[index] = gift
+
+    if command[0] == "JustInCase":
+        gift = command[1]
+        input_lines[-1] = gift
+
+    command_lines = input()
+    command = command_lines.split()
+
+for element in input_lines:
+    if "None" in input_lines:
+        input_lines.remove("None")
+print(*input_lines)
