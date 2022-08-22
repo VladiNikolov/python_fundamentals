97fa4ed751984750d60f2857d7b8d469fb58b63d
VladiNikolov <vladimir.pl.nikolov@gmail.com>
2022-08-22 20:00:42 +0300

    Create 03.numbers_.py

---
+++ exams_exercise/03.numbers_.py
@@ -0,0 +1,13 @@
+input_line = list(map(int, input().split()))
+average_number = sum(input_line) // len(input_line)
+new_list = []
+for index in range(len(input_line)):
+    if input_line[index] > average_number:
+        new_list.append(input_line[index])
+
+if new_list == []:
+    print("No")
+new_list.sort()
+new_list.reverse()
+print(*new_list[0:5])
+
